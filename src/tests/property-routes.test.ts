1860eb81b1a05ad0def856a34207c9e108a673f6
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2020-11-29 17:06:54 -0300

    feat: ensure Property route return a calc on success

---
+++ src/tests/property-routes.test.ts
@@ -0,0 +1,13 @@
+import request from 'supertest'
+import app from '../main/config/app'
+
+describe('Property Routes', () => {
+	test('Should return a calc of property on sucess',async () => {
+		await request(app)
+		.post('/api/property/calc')
+		.send({
+			squareMeters: 200
+		})
+		.expect(200)
+	})
+})
