b1a82a7ec213c3cf0bfe22bf48923b1bf53b4aca
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2020-11-29 21:03:17 -0300

    chore: config app port heroku

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,4 @@
 export default {
 	mongoUrl: process.env.MONGO_URL || 'mongodb://0.0.0.0:27017/app',
-	port: 5000,
-	user: 'ingaialab',
-	password: 'ingailab_2020',
-	mongoUrlProd: 'mongodb+srv://ingaialab:ingailab_2020@cluster0.eah1o.mongodb.net/app?retryWrites=true&w=majority'
+	port: 5000
 }
--- src/main/server.ts
+++ src/main/server.ts
@@ -4,8 +4,8 @@ import env from './config/env'
 MongoHelper.connect(env.mongoUrl)
 .then(async () => {
 	const app = (await import('./config/app')).default
-	app.listen(env.port, () => {
-		console.log(`Server running at http://localhost:${env.port}`)
+	app.listen(process.env.PORT || env.port, () => {
+		console.log(`Server running at http://localhost:${env.port} for dev`)
 	})
 })
 .catch(console.error)
